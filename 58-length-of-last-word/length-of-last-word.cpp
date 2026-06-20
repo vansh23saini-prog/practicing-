30e6e0d3ae8d5700be8bbdfe69eea1dfc4197d6f
Vansh Saini <vansh23saini@gmail.com>
2026-06-20 15:42:25 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (33.39%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+    int cnt = 0;
+    int i = s.size()-1;
+
+    while(i>=0 && s[i]==' '){
+        i--;
+    }  
+
+    while(i>=0 && s[i]!=' '){
+        cnt++;
+        i--;
+    }
+
+    return cnt;
+    }
+};
