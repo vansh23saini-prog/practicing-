fc15f584bdb8964d66f7197d8d00b78a6f27b978
Vansh Saini <vansh23saini@gmail.com>
2026-06-20 14:58:19 +0530

    Time: 603 ms (35.09%) | Memory: 295.6 MB (20.91%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -4,9 +4,59 @@ class Solution
 public:
     vector<int> sortArray(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
+
+private:
+    void merge(vector<int> &nums, int low, int mid, int high)
+    {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
     
+        while (left <= mid && right <= high)
+        {
+            if (nums[left] <= nums[right])
+            {
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else
+            {
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid)
+        {
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while (right <= high)
+        {
+
+            temp.push_back(nums[right]);
+            right++;
+        }
+        // modify in place
+        for (int i = low; i <= high; i++)
+        {
+            nums[i] = temp[i - low];
+        }
+    }
+
+    void mergeSort(vector<int> &nums, int low, int high)
+    {
+        if (low == high)
+        {
+            return;
+        }
+        int mid = (low + high) / 2;
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
+
+        merge(nums, low, mid, high);
+    }
 };
