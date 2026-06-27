e6cae3a6067b668650e5674e680c585be7e5c403
Vansh Saini <vansh23saini@gmail.com>
2026-06-27 14:09:00 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (99.15%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        for(int i = 0; i < k; ++i) {
+            auto minIt = min_element(nums.begin(), nums.end());
+            *minIt *= multiplier;
+        }
+        return nums;
+    }
+};
